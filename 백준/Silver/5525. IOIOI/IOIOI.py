adf43b2045c2f39dc7e90d66b491777f8c9449fe
Daeyeon Lee <angellee98@naver.com>
2025-12-07 18:08:22 +0900

    [Silver I] Title: IOIOI, Time: 244 ms, Memory: 35344 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,13 +1,19 @@
 N = int(input())
-M = int(input())
+M = int(input()) # S의 길이
 S = input()
-
-
 search ='I' + 'OI'* N
 
 
-cnt = 0
-for i in range(M-len(search)+1):
-    if S[i:i+len(search)] == search:
-        cnt += 1
-print(cnt)
+i = 0 # 자릿수
+cnt = 0 # IOI의 갯수
+ans = 0 # 실제 search 개수
+while i < M - 2:
+    if S[i] == "I" and S[i+1] == "O" and S[i+2] == "I":
+        cnt += 1 # IOI 개수 세기
+        if cnt >= N:
+            ans += 1 # search 개수 세기
+        i += 2 # 세글자씩 보면 되니까
+    else:
+        cnt = 0 # IOI 구조가 깨지면
+        i += 1
+print(ans)
