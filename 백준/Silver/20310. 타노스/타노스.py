b7ec813258e7594b61aa414108377e6511528ffc
Daeyeon Lee <angellee98@naver.com>
2025-12-15 12:29:03 +0900

    [Silver III] Title: 타노스, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,14 +1,35 @@
-S = input()
+import sys
+S = sys.stdin.readline().strip()
+
 
 cnt_v = {}
 
 for letter in S:
     cnt_v[letter] = cnt_v.get(letter,0) + 1
 
+# 0인 경우엔 0으로
+remove_1 = cnt_v.get('1',0) // 2
+remove_0 = cnt_v.get('0',0) // 2
 
-ans = ''
-sorted_cnt = sorted(cnt_v.items())
 
-for num, cnt in sorted_cnt:
-    ans += num * (int(cnt) // 2)
-print(ans)
+temp = []
+
+# 앞에서부터 1 절반 제거
+for l in S:
+    if l == '1' and remove_1 > 0:
+        remove_1 -= 1
+        continue
+    else:
+        temp.append(l)
+
+ans = []
+# 뒤에서부터 0 절반 제거
+for l in reversed(temp):
+    if l == '0' and remove_0 > 0:
+        remove_0 -= 1
+        continue
+    else:
+        ans.append(l)
+
+
+print(''.join(reversed(ans)))
