516f8932631e71c2f7dc3f329b5a708635eee9ee
Daeyeon Lee <angellee98@naver.com>
2026-01-07 11:26:38 +0900

    [Silver III] Title: 걸그룹 마스터 준석이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.py
+++ 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.py
@@ -8,13 +8,12 @@ for _ in range(N):
         member = input()
         group.setdefault(team,[]).append(member)
 
-
 for _ in range(M):
     problem = input()
     problem_num = int(input())
 
     if problem_num == 0:
-        ans = group[problem]
+        ans = group.get(problem)
         ans.sort()
         print('\n'.join(ans))
     if problem_num == 1:
