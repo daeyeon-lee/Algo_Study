c17b0f57d995fc5d4d26d5c43fd308897b5349f9
Daeyeon Lee <angellee98@naver.com>
2026-02-15 20:02:17 +0900

    [Silver III] Title: 합 구하기, Time: 192 ms, Memory: 50512 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.py
+++ 백준/Silver/11441. 합 구하기/합 구하기.py
@@ -1,10 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 nums = list(map(int,input().split()))
 M = int(input())
 sum_v = [0] * (N+1)
 for i in range(N):
     sum_v[i+1] = nums[i] + sum_v[i]
-
+ans = []
 for _ in range(M):
     i, j = map(int,input().split())
-    print(sum_v[j]-sum_v[i-1])
+    ans.append(sum_v[j]-sum_v[i-1])
+print('\n'.join(map(str,ans)))
